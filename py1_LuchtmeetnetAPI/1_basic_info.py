dcdabfa072bf604973747f1625330f2fdd19cd10
Dr. Maurice de Kleijn <mtm.de.kleijn@gmail.com>
2020-08-19 17:18:47 +0200

    Update 1_basic_info.py

--- py1_LuchtmeetnetAPI/1_basic_info.py
+++ py1_LuchtmeetnetAPI/1_basic_info.py
@@ -2,25 +2,21 @@
 import requests
 
 
-# First we will requests a list of all available stations.
-# Find the suitable URL and request data using requests
+# In order to understand how the luchtmeetnet API works, we are first going to create a list of all available stations. 
+# Go to https://documenter.getpostman.com/view/1562017/RVnbBxf9 and search for the suitable URL and request the data using requests.
+
+
+# In order to access the various data nodes we are going to create a python dictionary from the response. 
 
 
 
-
-# Create a python dictionary from the response
-
-
-
-
-
-# Explore the response using the variable explorer of Spyder or by printing it out to console.
+# Now explore the response using the variable explorer of Spyder or by printing it out to console.
 # Save the first location to a variable and print it out the the console.
 
 
 
 
-# Find a station that is within Amsterdam.
+# Find a station that is within Amsterdam (or any other city you prefer).
 # Save location name and station number to variables
 # and print out to console
 
@@ -28,16 +24,14 @@ import requests
 
 
 # Request the data of your chosen station and create a python dictionary
-# Hint: you need to specify the station number in the URL
-# Check the API documentation for an example.
-# Use the variable for the station number created just before.
+# Hint: you need to specify the station number in the URL. In order to see how that is done please check the API documentation for an example.
+# Now use the variable for the station number created just before.
 
 
 
 
 # Save the description, the chemical compounds which are measured at this
-# station and the location (coordinates) to variables
-# And print out to console.
+# station and the location (coordinates) to variables and print the result.
 
 
 

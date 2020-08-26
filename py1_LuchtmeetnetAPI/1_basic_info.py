26f4d5158b028754f0a3651f557684d85341d44e
Dr. Maurice de Kleijn <mtm.de.kleijn@gmail.com>
2020-08-26 10:04:34 +0200

    Update 1_basic_info.py

--- py1_LuchtmeetnetAPI/1_basic_info.py
+++ py1_LuchtmeetnetAPI/1_basic_info.py
@@ -1,38 +1,45 @@
-# documentation: http://docs.python-requests.org/en/master/
+# This is Excercise 1 for the practical assignment to learn python for API´s prepared by the Spatial Information Laboratory - Vrije Universiteit Amsterdam
+###################
+# Since we want to be able to get data out of the luchtmeetnet we first we import the request module (which we already did for you) 
+# documentation on the request module : http://docs.python-requests.org/en/master/
+
 import requests
 
 
-# In order to understand how the luchtmeetnet API works, we are first going to create a list of all available stations. 
-# Go to https://documenter.getpostman.com/view/1562017/RVnbBxf9 and search for the suitable URL and request the data using requests.
+# In order to understand how the luchtmeetnet API works, we you to go through the following steps. 
+# Step 1: First we need to find the URL with which we can access the API and get to the different data nodes. Go to https://documenter.getpostman.com/view/1562017/RVnbBxf9 and search for the right URL.
+# Add this URL to a get request. Put this request in the line below:
 
 
-# In order to access the various data nodes we are going to create a python dictionary from the response by reading the json API output. 
+# Step 2: In order to access the various data nodes we now want you to create a python dictionary from the response by reading the API output. You probably noticed the API generates json output so make sure to read json. 
+# Write your line(s) of code below:
 
 
-
-# Now explore the response using the variable explorer of Spyder or by printing it out to console.
-# Save the first location to a variable and print it out the the console.
-
-
-
-
-# Find a station that is within Amsterdam (or any other city you prefer).
-# Save location name and station number to variables
-# and print out to console
+# Now that the API is added to a dictionary you can explore the response using the variable explorer of Spyder or by printing it out to console.
+# Step 3: In this step we want you to save the first location to a variable and print it out the the console.
 
 
 
+# Step 4: Now go through th dictionary and select a station that is within Amsterdam (or any other city you prefer).
+# Save location name and station number to variables and print them out to console.
+# Write your line(s) of code below:
 
-# Request the data of your chosen station and create a python dictionary
-# Hint: you need to specify the station number in the URL. In order to see how that is done please check the API documentation for an example.
+
+
+# Now that we isolated one station we want you to get specific air quality measurements from that station. 
+# Step 5: In order to do so we want you to request the data of your chosen station and create a python dictionary.
+# Hint: you need to specify the station number in the URL. In order to see how that is done please check the API documentation (https://documenter.getpostman.com/view/1562017/RVnbBxf9) for an example.
 # Now use the variable for the station number created just before.
+# Write your line(s) of code below:
 
 
 
-
-# Save the description, the chemical compounds which are measured at this
-# station and the location (coordinates) to variables and print the result.
-
+# Step 6: Since we, as Geo Information students, are interested in locations we want you to save:
+# 1. the description
+# 2. the chemical compounds which are measured at this station
+# 3. the location (coordinates) to variables 
+# print the results.
+# Write your line(s) of code below:
 
 
 

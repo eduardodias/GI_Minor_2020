69c3b3850725e7c3b66297045c9b86c9a6defef3
DKBrands <69958781+DKBrands@users.noreply.github.com>
2020-09-07 10:43:07 +0200

    Update 1_basic_info.py

--- py1_LuchtmeetnetAPI/1_basic_info.py
+++ py1_LuchtmeetnetAPI/1_basic_info.py
@@ -10,18 +10,17 @@
 import requests
 
 
-# In order to understand how the luchtmeetnet API works, we you to go through the following steps. 
+# In order to understand how the luchtmeetnet API works, we want you to go through the following steps. 
 # Step 1: First we need to find the URL with which we can access the API and get to the different data nodes. Go to https://documenter.getpostman.com/view/1562017/RVnbBxf9 and search for the right URL.
-# Add this URL to a get request. Put this request in the line below:
+# Add this URL to a get request that you store as a response object. Put this request in the line below:
 
 
-# Step 2: In order to access the various data nodes we now want you to create a python dictionary from the response by reading the API output. You probably noticed the API generates json output so make sure to read json. 
+# Step 2: In order to access the various data nodes we now want you to create a python dictionary from the response object by reading the API output. You probably noticed the API generates json output so make sure to read json. 
 # Write your line(s) of code below:
 
 
-# Now that the API is added to a dictionary you can explore the response using the variable explorer of Spyder or by printing it out to console.
-# Step 3: In this step we want you to save the first location to a variable and print it out the the console.
-
+# Now that the response from the API is added to a dictionary you can explore the response using the variable explorer of Spyder or by printing it out to console.
+# Step 3: In this step we want you to save the first location to a variable and print it out to the console.
 
 
 # Step 4: Now go through th dictionary and select a station that is within Amsterdam (or any other city you prefer).
@@ -29,13 +28,11 @@ import requests
 # Write your line(s) of code below:
 
 
-
 # Now that we isolated one station we want you to get specific air quality measurements from that station. 
 # Step 5: In order to do so we want you to request the data of your chosen station and create a python dictionary.
 # Hint: you need to specify the station number in the URL. In order to see how that is done please check the API documentation (https://documenter.getpostman.com/view/1562017/RVnbBxf9) for an example.
-# Now use the variable for the station number created just before.
+# Now use the variable for the station number created in step 4.
 # Write your line(s) of code below:
-
 
 
 # Step 6: Since we, as Geo Information students, are interested in locations we want you to save:
@@ -44,8 +41,6 @@ import requests
 # 3. the location (coordinates) to variables 
 # print the results.
 # Write your line(s) of code below:
-
-
 
 
 

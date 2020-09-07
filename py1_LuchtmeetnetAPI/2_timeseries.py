a408462e8a16106e32c3f973e2d8310cccbb0711
DKBrands <69958781+DKBrands@users.noreply.github.com>
2020-09-07 11:52:22 +0200

    Update 2_timeseries.py

--- py1_LuchtmeetnetAPI/2_timeseries.py
+++ py1_LuchtmeetnetAPI/2_timeseries.py
@@ -4,7 +4,7 @@
 # Simeon Nedkov, Chris Lucas, Maurice de Kleijn, Devi Brands                                  #
 ###############################################################################################
 
-# For this excercise we are going look into the air measurements for a specific station for a specific chemical component.
+# For this excercise we are going look into the air measurements for a specific station for a specific chemical compound.
 # Fot this we will be using the following modules. The first one you are already familiar with from the first exercise. 
 # The other´s will be explained once you are going to use them for your code
 import requests
@@ -20,10 +20,10 @@ import matplotlib.pyplot as plt
 # Step 2: Choose a chemical compound and request the data (no need to specify a page yet, you will by default recieve the most recent page)
 # Create a dictionary of the response in the same way you did in the previous exercise.
 # hint: you need to specify the station number in the URL
-# hint: you need to specify the chemical compound in the parameters of the request (http://docs.python-requests.org/en/master/user/quickstart/#passing-parameters-in-urls)
+# hint: you need to specify the chemical compound in the parameters of the request (https://requests.readthedocs.io/en/master/user/quickstart/#passing-parameters-in-urls)
 # hint: look in the API documentation what the parameter to define a chemical compound is called (https://documenter.getpostman.com/view/1562017/RVnbBxf9)
 # Write your lines of code below:
-
+    
 
 
 # Step 3: Now that we have the specific component we are going to 
@@ -31,7 +31,7 @@ import matplotlib.pyplot as plt
 # Create a list of corresponding timestamps
 # hint: look in the data where the measured quantities and timestamps are stored
 # hint: you will need to loop over the retrieved data
-# hint: create empty lists and append the data to the lists in the loop (look back you training material if you forgot how)
+# hint: create empty lists and append the data to the lists in the loop (check your training material if you forgot how)
 # Write your lines of code below:
 
 
@@ -40,7 +40,7 @@ import matplotlib.pyplot as plt
 # You probably noted that the timestamps are strings that are build in a certain way (a format)
 # Before we can plot the data we therefore need to tell the computer how to interpret this string
 # and create a datetime object with it that python can interpret
-# (As promised above we would inform you when one of the modules above is used. At this point we are using datetime.)
+# (As promised above we would inform you when one of the modules above is used. At this point we are going to use datetime.)
 # For simplicity we give you the correct time format: 
 # REMOVE THE HASHTAG IN THE NEXT LINE 
 #time_format = '%Y-%m-%dT%H:%M:%S+00:00'
@@ -50,8 +50,7 @@ import matplotlib.pyplot as plt
 
 
 
-
-# Step 5: Now we want you to plot the data in a graph. For this we want you to use matplotlib module (which we imported above). 
+# Step 5: Now we want you to plot the data in a graph. For this we want you to use the matplotlib module (which we imported above). 
 # We want you to plot timestamps on the X-axis and values for the chemical components on the Y-axis. 
 # Please look at https://pythonspot.com/plot-time-with-matplotlib/ on how to do so.
 # Write your lines of code below:
@@ -74,22 +73,21 @@ import matplotlib.pyplot as plt
 
 
 
-
-
 # Step 8: it might be that the graph looks wrong in some places now
 # this is due the data not being sorted by date
 # to sort the data by date we are going to create a DataFrame using the pandas module (which we imported as pd above)
 # a DataFrame is a datatype that stores tabular data, kinda like an excel sheet
 # https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html
-# To get you started we have prepared a couple of line to initaite a DataFrame:
+# To get you started we have prepared a couple of lines to initaite a DataFrame:
 # REMOVE THE HASHTAG IN THE NEXT LINE
-#df = pd.DataFrame()
-#df['timestamps'] = timestamps
-#df['values'] = values
+# df = pd.DataFrame()
+# df['timestamps'] = timestamps
+# df['NO2'] = values
 
 # now that the data is in a DataFrame we can use the sort_values function of the DataFrame to sort the values by the timestamps 
 # Look here how that is done https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.sort_values.html
 # write the sort function for timestaps in the line below:
+
 
 
 # Step 9: pandas DataFrames also come with easy function to make a plot, use it to create a plot of the sorted data
@@ -102,3 +100,4 @@ import matplotlib.pyplot as plt
 # To do so we have prepared the following line. 
 # REMOVE THE HASHTAG IN THE NEXT LINE
 #df.to_csv('timeseries.csv')
+
